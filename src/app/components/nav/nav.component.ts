3964f5e02e11d6e8b10c2e4fbc57f2538f50b7ba
DiegoAmbrocio <diehgo2.229@gmail.com>
2023-06-27 18:31:07 -0600

    Routing y pagina de detalle de producto

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { StoreService } from 'src/app/services/store.service';
 import { User } from '../models/user.model';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-nav',
@@ -12,23 +14,33 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categorias: Category[] = [];
 
   constructor(
     private storeService: StoreService,
-    private authService: AuthService
+    private authService: AuthService,
+    private categoriesService: CategoriesService
   ) {}
   ngOnInit(): void {
     this.storeService.myCart$.subscribe((products) => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
   }
   login() {
-    this.authService.loginAndGet('Diego@gmail.com', '666').subscribe((user) => {
-      this.profile = user;
+    this.authService
+      .loginAndGet('diego@gmail.com', '12345')
+      .subscribe((user) => {
+        this.profile = user;
+      });
+  }
+  getAllCategories() {
+    this.categoriesService.getAll().subscribe((data) => {
+      this.categorias = data;
     });
   }
 }
